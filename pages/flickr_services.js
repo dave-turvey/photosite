93aecabade4220bdb20a5fc3dbdb87dad5822d10
dave-turvey <>
2013-06-24 12:28:33 +0100

    Initial working version

---
+++ pages/flickr_services.js
@@ -0,0 +1,47 @@
+var photosetID='72157622478698869';
+//API key for use in the calls
+var apiKey = '83579700317fd9a3baaadcf305b61c5b';
+var url = "http://api.flickr.com/services/rest/?method=flickr.galleries.getList&api_key="+apiKey+"&user_id=30527111%40N05&format=json&jsoncallback=?";
+var $imagesLoaded = false;
+
+function getPhotoList(data,textStatus,jqXHR)
+{
+	var imglist="<ul>";
+	var ptar = data.photoset.photo;
+	for (var img in ptar) {
+
+		imglist+= "<li><img src=\"http://farm"+ptar[img].farm+".staticflickr.com/"+ptar[img].server+"/"+ptar[img].id+"_"+ptar[img].secret+"_z.jpg\" width=800px height=800px></li>";
+	}
+	imglist+=$('#slider').html(imglist);
+	$('#imageList').html("imglist");
+	setUpSlider();
+	$imagesLoaded = true;
+	
+}
+
+function getGallery(data,textStatus,jqXHR)
+{
+	console.log("getGallery:"+Date.now());
+	var url = "http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key="+apiKey+"&photoset_id="+photosetID+"&format=json&jsoncallback=?";
+	$.getJSON(url,getPhotoList);
+	console.log("getGallery complete:"+Date.now());
+}
+
+
+
+//the initial json request to flickr to get the gallery ID
+//seeemilyplayuk userid = 30527111@N05
+// landscapeset=72157622478698869
+// structureset=72157620295497121
+// fenlandset=72157621963307915
+function changeSet(event)
+{
+    photosetID=$(this).attr("data-href");
+    $.getJSON(url,getGallery);    
+}
+
+$('.setselector').click(changeSet);
+$.getJSON(url,getGallery);
+
+ 
+	
---
+++ pages/slider-x.js
@@ -0,0 +1,37 @@
+var $slider = $('.slider'); // class or id of carousel slider
+var $slide = 'li'; // could also use 'img' if you're not using a ul
+var $transition_time = 2000; // 1 second
+var $time_between_slides = 8000; // 8 seconds
+var $i=-1;
+var $intervalID;
+
+function slides(){
+	return $slider.find($slide);
+}
+
+function timerCallback()
+{
+	var s = slides();
+    s.eq($i).removeClass('active');
+ 
+	s.eq($i).fadeOut($transition_time);
+	if ((s.length-1) == $i) 
+	{
+		$i = -1; // loop to start
+	}
+	$i++;
+	s.eq($i).fadeIn($transition_time);
+    s.eq($i).addClass('active');
+}
+		
+function setUpSlider()
+{
+	// Fadeout all the slides
+	slides().fadeOut();
+	// set active classes
+	slides().eq(0).addClass('active');
+	slides().eq(0).fadeIn($transition_time);
+	$slider.find($slide + '.active').index();
+	timerCallback();	// Place the initial slide on the screen before we start the timer to change them
+	setInterval(timerCallback, $transition_time +  $time_between_slides);
+}
